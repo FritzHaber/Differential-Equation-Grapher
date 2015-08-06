192a0f62fdd5af0124198cc94a2bcb448caae479
Hank <hank.obrien900@icloud.com>
2015-08-06 00:52:00 -0700

    NLP now parses power towers (e^x^2) as e^(x^2), and accounts for variables and logs

--- DiffEq Grapher FX/src/diffEqGrapherFX/InputParser.java
+++ DiffEq Grapher FX/src/diffEqGrapherFX/InputParser.java
@@ -8,45 +8,55 @@ package diffEqGrapherFX;
  * @author Hank O'Brien
  **/
 public class InputParser {
-	//TODO: exponents, e^x, constants (e, pi, etc)
-	//TODO: logs
-//	private static String inputEq = "(5)^(6)*7";
-//	private String processedEq;
 	private static char var1 = 'x';
 	private static char var2 = 'y';
 	
-//	private InputParser(String rawInput){
-//		this.inputEq = rawInput;
-//		//parse();
-//	}
-	
-//	public static void main(String[] args){
-//		//parse();
-//		//System.out.println(inputEq);
-//	}
 	
 	//used externally
 	public static String parse(String natEq){
-		natEq = parseImplicitMultiplication(natEq);
-		natEq = parseTrig(natEq);
-		natEq = parseExponents(natEq);
-		return natEq;
+		natEq = natEq.replaceAll("\\s",""); //removes whitespace
+		String temp = natEq;
+
+		temp = parseConstants(temp);
+		temp = parseTrig(temp);
+		temp = parseLogs(temp);
+		do{
+			natEq = temp;
+			temp = parseImplicitMultiplication(temp);
+			temp = parseExponents(temp);
+		}while(!natEq.equals(temp));
+
+		return temp;
 	}
 	
+	private static String parseLogs(String natEq) {
+		natEq = natEq.replace("log(", "Math.log10(");
+		natEq = natEq.replace("ln(", "Math.log(");
+		return natEq;
+	}
+
+	//handles e and pi
+	private static String parseConstants(String natEq) {
+		natEq =  natEq.replace("pi", "(" + Math.PI + ")");
+		natEq = natEq.replace("e", "(" + Math.E + ")");
+		return natEq;
+	}
+
 	private static String parseImplicitMultiplication(String natEq) {
 		natEq = stage1Parse(natEq);
 		natEq = stage2Parse(natEq);
 		natEq = stage3Parse(natEq);	
 		return natEq;
 	}
-	//has issues with multiple consecutive parenthesis (2*(3))^(8)
-	//if user input is mathematically correct, the only characters that can border the ^ sign is (, ), or the digits
+
+	//if user input is mathematically correct, the only characters that can border the ^ sign is (, ), or the digits, or trig functions
 	private static String parseExponents(String natEq) {
 		//assume ^ is not at index 0, or at index length
 		if(natEq.contains("^")){
-			int i = natEq.indexOf("^");
+			int i = indexOfFromBack(natEq, '^');
+			
+			//term below power
 			int firstMarker = i-1;
-
 			if(natEq.charAt(i-1) == ')'){
 				int parenCount = 0;
 				do{
@@ -61,85 +71,115 @@ public class InputParser {
 					//over compensates in this case
 					if(firstMarker == 0)
 						firstMarker--;
+					if(parenCount == 0){
+						if(Character.isAlphabetic(natEq.charAt(firstMarker))){
+							while(natEq.charAt(firstMarker) != 'M'){
+								firstMarker--;
+							}
+							firstMarker--;
+						}
+					}
 				}while(parenCount > 0 && firstMarker > 0);
 				//compensates for additional decrement before loop termination if firstMarker does not end at 0
 				firstMarker++;
+			}else if(natEq.charAt(firstMarker) == var1 || natEq.charAt(firstMarker) == var2){ //case of x^3 or y^3
+				//do nothing, firstMarker is at a good location
 			}else{ //assume number in this case?
-				while(firstMarker > 0 && Character.isDigit(natEq.charAt(firstMarker))){
+				while(firstMarker > 0 && isDigit(natEq.charAt(firstMarker))){
 					firstMarker--;
 				}
-				
+				firstMarker++;
 			}
 			
-			
-			int lastMarker = i+1;
-			if(natEq.charAt(lastMarker) == '('){
+			//power
+			int endMarker = i+1;
+			if(natEq.charAt(endMarker) == '('){
 				int parenCount = 0;
 				do{
-					if(natEq.charAt(lastMarker) == ')'){
+					if(natEq.charAt(endMarker) == ')'){
 						parenCount--;
 					}
-					if(natEq.charAt(lastMarker) == '('){
+					if(natEq.charAt(endMarker) == '('){
 						parenCount++;
 					}
-					lastMarker++;
-				}while(parenCount > 0 && lastMarker < natEq.length());
-				lastMarker--;
+					endMarker++;
+				}while(parenCount > 0 && endMarker < natEq.length());
+				endMarker--;
+			}else if(natEq.charAt(endMarker) == var1 || natEq.charAt(endMarker) == var2){
+				endMarker = i + 2;
+			}else if(natEq.charAt(endMarker) == 'M'){ //possibly means exponent or log power
+				while(natEq.charAt(endMarker) != ')'){
+					endMarker++;
+				}
 			}else{ //assume number
-				while(lastMarker < natEq.length() && Character.isDigit(natEq.charAt(lastMarker))){
-					lastMarker++;
+				while(endMarker < natEq.length() && isDigit(natEq.charAt(endMarker))){
+					endMarker++;
 				}
 			}
 			
 			String nonExpFirst = natEq.substring(0,firstMarker);
-			String nonExpLast = natEq.substring(lastMarker, natEq.length());
+			String nonExpLast = natEq.substring(endMarker, natEq.length());
 			String term = natEq.substring(firstMarker, i);
-			String power = natEq.substring(i+1, lastMarker);
+			String power = natEq.substring(i+1, endMarker);
 			natEq = nonExpFirst + "Math.pow(" + term + ","  + power + ")" + nonExpLast;
 		}
 		return natEq;
 	}
 
-//	private static int getPrecedence(char c) {
-//		//order of precedence follows Java's operator precedence order
-//		if(c == '(' || c == ')')
-//			return 3;
-//		else if(c == '*' || c == '/' || c == '%')
-//			return 2;
-//		else if(c == '+' || c == '-')
-//			return 1;
-//		else
-//			//case for c is a number, hence not an operator
-//			return 4;
-//	}
+	
+	private static int indexOfFromBack(String s, char c){
+		for(int i = s.length() - 1; i >= 0; i--){
+			if(s.charAt(i) == c)
+				return i;
+		}
+		return -1;
+	}
+	
+	//need to have the decimal case, because decimals are always part of a number
+	private static boolean isDigit(char c) {
+		if(Character.isDigit(c) || c == '.')
+			return true;
+		else
+			return false;
+	}
 
 	private static String parseTrig(String natEq) {
-		natEq = natEq.replace("arcsin*(", "Math.asin(");
-		natEq = natEq.replace("arccos*(", "Math.acos(");
-		natEq = natEq.replace("arctan*(", "Math.atan(");
-		//multiplication sign because multiplication parse things sin is multiplied by (, so it adds a multiplication symbol
-		natEq = natEq.replace("sin*(", "Math.sin(");
-		natEq = natEq.replace("cos*(", "Math.cos(");
-		natEq = natEq.replace("tan*(", "Math.tan(");
+		natEq = natEq.replace("arcsin(", "Math.asin(");
+		natEq = natEq.replace("arccos(", "Math.acos(");
+		natEq = natEq.replace("arctan(", "Math.atan(");
+		natEq = natEq.replace("sin(", "Math.sin(");
+		natEq = natEq.replace("cos(", "Math.cos(");
+		natEq = natEq.replace("tan(", "Math.tan(");
 		return natEq;
 	}
 
 	//parses 6x to 6*x
 	private static String stage3Parse(String natEq) {
 		for(int i = 1; i < natEq.length(); i++){
-			if(Character.isDigit(natEq.charAt(i)) && isVariable(natEq.charAt(i-1))){
+			if(isDigit(natEq.charAt(i)) && isVariable(natEq.charAt(i-1))){		//2x -> 2*x
 				natEq = addMultiplicationSymbol(natEq, i);
 			}
-			if(Character.isDigit(natEq.charAt(i-1)) && isVariable(natEq.charAt(i))){
+			if(isVariable(natEq.charAt(i)) && isDigit(natEq.charAt(i-1))){		//x2 -> x*2
 				natEq = addMultiplicationSymbol(natEq, i);
 			}
-			if(isVariable(natEq.charAt(i)) && isVariable(natEq.charAt(i-1))){
+			if(isVariable(natEq.charAt(i)) && isVariable(natEq.charAt(i-1))){	//xy -> x*y
 				natEq = addMultiplicationSymbol(natEq, i);
+			}
+			if(isVariable(natEq.charAt(i)) && natEq.charAt(i - 1) == 'M'){
+				natEq = addMultiplicationSymbol(natEq,i);
+			}
+			if(natEq.charAt(i) == 'M' && isVariable(natEq.charAt(i - 1))){
+				natEq = addMultiplicationSymbol(natEq,i);
+			}
+			if(isDigit(natEq.charAt(i)) && natEq.charAt(i - 1) == 'M'){
+				natEq = addMultiplicationSymbol(natEq,i);
+			}
+			if(natEq.charAt(i) == 'M' && isDigit(natEq.charAt(i - 1))){
+				natEq = addMultiplicationSymbol(natEq,i);
 			}
 		}
 		return natEq;
 	}
-
 
 	private static boolean isVariable(char c) {
 		if(c == var1 || c == var2){
@@ -166,24 +206,20 @@ public class InputParser {
 	}
 	
 	//parses 6(x) to 6*(x), adds multiplication sign between non-operators and numbers
+	//M cases for Math.sin etc, enables processing of trig and log functions
 	private static String stage2Parse(String natEq){
 		for(int i = 1; i < natEq.length(); i++){
-			if(natEq.charAt(i) == '(' && (Character.isDigit(natEq.charAt(i-1)) || !isAnOpertor(natEq.charAt(i-1)))){
+			if((isDigit(natEq.charAt(i-1)) || natEq.charAt(i-1) == 'M') && natEq.charAt(i) == '('){		//6(x) -> 6*(x)
 				natEq = addMultiplicationSymbol(natEq, i);
 			}
-			if(natEq.charAt(i-1) == ')' && (Character.isDigit(natEq.charAt(i)) || !isAnOpertor(natEq.charAt(i)))){
+			if(natEq.charAt(i-1) == ')' && (isDigit(natEq.charAt(i)) || natEq.charAt(i) == 'M')){		//(x)6 -> (x)*6
 				natEq = addMultiplicationSymbol(natEq, i);
 			}
 		}
+		//fixes a small bug in the above code where a trig function was multiplied by another parenthesis, like cos(x)*)
+		natEq = natEq.replace("(*(", "((");
+		natEq = natEq.replace(")*)", "))");
 		return natEq;
 	}
 	
-	private static boolean isAnOpertor(char c) {
-		String operators = "+-*/%^";
-		return operators.contains(c + "");
-	}
-
-//	public String getProcessedString(){
-//		return processedEq;
-//	}
 }
